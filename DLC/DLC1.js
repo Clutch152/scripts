42f6ff70ce9dc43acec2f94de3c5d6a4a04e3c57
rebussharma <rebussharma@gmail.com>
2020-04-25 15:50:25 -0500

    Added scriopts for DLC1, updated SSD1

---
+++ DLC/DLC1.js
@@ -0,0 +1,6 @@
+window.opener.scorm.set("cmi.completion_status","completed");
+window.opener.scorm.set("cmi.success_status","passed");
+window.opener.scorm.set("cmi.score.raw",90);
+window.opener.scorm.set("cmi.score.scaled",0.9);
+window.opener.scorm.commit();
+window.close();
