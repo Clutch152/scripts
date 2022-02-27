ed10e3a47a15495b75c159e206f3bf8fe920dd52
TyrantRex <TyrantRex@users.noreply.github.com>
2022-02-26 20:56:23 -1000

    Update skillport_breaker.user.js

--- Skillport/skillport_breaker.user.js
+++ Skillport/skillport_breaker.user.js
@@ -1,58 +1,112 @@
 // ==UserScript==
-// @name         Break SkillPort
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Skillport Bypass
-// @author       iEatNoodlez_
-// @match        *://*.skillport.com/*
+// @name         MVP Skillport Cheats
+// @namespace    http://127.0.0.1/
+// @version      3.0
+// @description  Updated 022230172022
+// @author       TyrantRex
+// @match        *://library.skillport.com/*
 // ==/UserScript==
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                                               *
- * Directions:                                                                   *
- * 1. Install Greasemonkey (Google Chrome) or Tampermonkey (Firefox)             *
- * 2. Click "Raw" in the top right of this box.                                  *
- * 3. Press Button or press "S" once the course has launched and loaded          *
- *    completely.                                                                *
- * 4. Use your mouse to click each of the items in the Table of Contents twice.  *
- *    Green progress circle will fill in with each click.                        *
- * 5. Press Button or "S" when you get to the test to get a passing grade.       *
- *                                                                               *
+ * Directions:
+ * 1. Take note of the course name
+ * 2. Launch it and go to the table of contents
+ * 3. Click through each of the items in the Table of Contents.
+ *    -- You want that dotted white circle to be half green
+ * 4. Press TAB when you get to the test
+ * 5. Close the window and launch the course again.
+ * 6. Click through any new topics that may have popped up
+ * 7. Press TAB at the then refresh by clicking on the test page to finish up
+ *
+ * It's not perfect but will work as a minimum viable product
+ *
+ * You'll see two buttons under the "Save & Exit" link. (I was inspired after
+ * forking the repository and stealing it from iEatNoodlez_)
+ *
+ * [>>] Will speed up the videos
+ * [>|] Will attempt to actually let you skip the video by setting current
+ *      progress right before the end of the video.
+ *
+ * The second button is experimental. Well, my TIS is coming to an end as well, but
+ * this is to pay it forward after picking up the torch from Clutch152 back in 2020
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * As always, use at your own risk. All liabilities are assumed by the user.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-(function() {
-    const btn = document.createElement('button');
-    btn.textContent = 'First and Last Run';
-    btn.style.zIndex = 100000;
-    btn.style.backgroundColor = '#fff';
-    btn.style.position = 'absolute';
-    btn.style.top = '50%';
-    btn.style.left = '50%';
-    btn.style.fontSize = "30px";
-    btn.style.cursor = "pointer";
-    document.body.appendChild(btn);
+$(document).ready(function() {
+    const btnFF = document.createElement("button");
+          btnFF.innerHTML = '>>';
+          btnFF.style.top = '50px';
+          btnFF.style.right = '40px';
+          btnFF.style.zIndex = 100000;
+          btnFF.style.position = 'absolute';
 
-    function FirstandLastRun() {
-        var a = Core.context.getResultsMgr().masterBucket.getTopicItems().data;
-        for (i = 0; i < a.length; i++) {
-            var b = a[i];
-            if (a[i].numberOfObjectives >= 0) {
-                b.pre = b.best = b.last = 100;
-                b.masteryStatus = 1;
-                b.visitStatus = 4;
-                b.firstAttemptIsPreassess = true;
+    const btnNext = document.createElement("button");
+          btnNext.innerHTML = '>|';
+          btnNext.style.top = '50px';
+          btnNext.style.right = '10px';
+          btnNext.style.zIndex = 100000;
+          btnNext.style.position = 'absolute';
+
+    function RunExploit() {
+        var courseDataArray = Core.context.course.allTopics,
+            randomScore = 0;
+
+        for (var x = 0; x < courseDataArray.length; x++) {
+            var courseData = courseDataArray[x].resultsMgr.masterBucket,
+                courseInfo = courseData.getTopicStatusItems(),
+                index = courseData.courseTitle + "_" + courseDataArray[x].id;
+
+            randomScore = Math.floor(Math.random() * (100-80) + 80);
+
+            if (courseInfo.table[index].numberOfObjectives >= 0) {
+                courseInfo.table[index].pre = courseInfo.table[index].best = courseInfo.table[index].last = randomScore;
+                courseInfo.table[index].masteryStatus = 1;
+                courseInfo.table[index].visitStatus = 4;
+                courseInfo.table[index].firstAttemptIsPreassess = true;
+
+                randomScore = 0;
             }
         }
-        Core.context.getResultsMgr().masterBucket.updateAllLessonScores();
-        Core.context.getResultsMgr().masterBucket.updateAllLessonStatus();
+
+        Core.context.course.getResultsMgr().getMasterBucket().updateAllLessonScores();
+        Core.context.course.getResultsMgr().getMasterBucket().updateAllLessonStatus();
     }
 
-    btn.addEventListener('click', () => {
-        maybeSkipMedia();
-    });
-    document.addEventListener("keypress", ({key}) => {
-        if (key === 's') {
-            FirstandLastRun();
+    function FastFoward() {
+        for (let x = 0; x < 1000; x++) {
+            setTimeout(function() {document.getElementById("videoPlayer").playbackRate=16;}, 500 * x);
+        }
+    }
+
+    function Skip() {
+        Control.navMgr.getMenuPanel().getSettingsPanel().autoPlayBtn.selected = true;
+
+        var videoData = document.querySelector('video');
+
+        for (let i = 0; i < 1000; i++) {
+            setTimeout(function() {
+                if(videoData.currentTime) {
+                    videoData.currentTime = videoData.duration - 0.5;
+                }
+            }, 850 * i);
+        }
+    }
+
+    document.addEventListener("keydown", function(event){
+        if (event.code === "Tab") {
+            RunExploit();
         }
     });
+
+    btnFF.addEventListener('click', () => {
+        FastFoward();
+    });
+
+    btnNext.addEventListener('click', () => {
+        Skip();
+    });
+
+    document.body.appendChild(btnFF);
+    document.body.appendChild(btnNext);
 })();
